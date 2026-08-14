ce87e4ad1079a8e980ce860c8a942ecb3b5d66c9
vimalrajprakash <vimalrajprakash256@gmail.com>
2026-08-14 21:06:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+Hello from Vimal's AWS Continuous Delivery Pipeline!
